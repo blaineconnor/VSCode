ffcb3bab5c63499f50df43a842c360a2092d92ed
blaineconnor <fesarman@anadolu.edu.tr>
2023-11-17 17:59:28 +0300

    Localizations imported the javascript.

---
+++ Project/assets/js/language/language.js
@@ -0,0 +1,135 @@
+﻿let LJS_Language = "tr";
+
+const LJS_title_site = { "Ahlatcı Teknoloji - Kurumsal Yönetim Sistemi": "tr", "Ahlatcı Technology - Corporate Management System": "en", "Ahlatcı Tecnologia - Sistema di Gestione Aziendale": "it", "Ahlatcı Tecnología - Sistema de Gestión Corporativa": "es", "Ahlatcı Technik - Unternehmensführung System": "de" };
+const LJS_footer_site = {"© Kurumsal Yönetim Sistemi - Ahlatcı Teknoloji": "tr", "© Corporate Management System - Ahlatcı Technology": "en", "© Sistema di Gestione Aziendale - Ahlatcı Tecnologia": "it", "© Sistema de Gestión Corporativa - Ahlatcı Tecnología": "es", "© Unternehmensführung System - Ahlatcı Technik": "de" };
+
+const LJS_nav_welcome = { "Hoşgeldiniz": "tr", "Welcome": "en", "Benvenuto": "it", "Bienvenido": "es", "Willkommen": "de" };
+const LJS_nav_support = { "Destek": "tr", "Support": "en", "Supporto": "it", "Apoyar": "es", "Unterstützung": "de" };
+const LJS_nav_myAccount = { "Hesabım": "tr", "My Account": "en", "Mio Conto": "it", "Mi Cuenta": "es", "Mein Konto": "de" };
+const LJS_nav_logout = { "Çıkış Yap": "tr", "Logout": "en", "Disconnettersi": "it", "Cerrar Sesión": "es", "Abmelden": "de" };
+const LJS_nav_light = { "Işıklar": "tr", "Lights": "en", "Luci": "it", "Luces": "es", "Lichter": "de" };
+
+//Table Name
+const LJS_order = { "Sıra": "tr", "Order": "en", "Order2": "it", "Order3": "es", "Order4": "de" };
+const LJS_invoiceNo = { "Fatura No": "tr", "Invoice No": "en", "": "it", "": "es", "": "de" };
+const LJS_invoiceDate = { "Fatura Tarihi": "tr", "Incoice Date": "en", "": "it", "": "es", "": "de" };
+const LJS_supplier = { "Tedarikçi": "tr", "Supplier": "en", "": "it", "": "es", "": "de" };
+const LJS_print = { "Yazdır": "tr", "Print": "en", "": "it", "": "es", "": "de" };
+const LJS_product = { "Ürün": "tr", "": "en", "": "it", "": "es", "": "de" };
+const LJS_productName = { "Ürün Adı": "tr", "": "en", "": "it", "": "es", "": "de" };
+const LJS_quantity = { "Adet": "tr", "": "en", "": "it", "": "es", "": "de" };
+const LJS_unitQuantity = { "Miktar": "tr", "": "en", "": "it", "": "es", "": "de" };
+
+//Placeholder
+
+
+//Employe Layout Li
+
+const LJS_li_home = { "Anasayfa": "tr", "Home": "en", "Pagina Principale": "it", "Página Principal": "es", "Startseite": "de" };
+const LJS_li_apps = { "Uygulamalar": "tr", "Apps": "en", "Applicazioni": "it", "Aplicaciones": "es", "Anwendungen": "de" };
+const LJS_li_message = { "Mesajlaşma": "tr", "Message": "en", "Messaggio": "it", "Mensaje": "es", "Nachricht": "de" };
+
+const LJS_li_request = { "Talep": "tr", "Requests": "en", "Richieste": "it", "Solicitudes": "es", "Anfragen": "de" };
+const LJS_li_requestCreate = { "Talep Oluştur": "tr", "Create Request": "en", "Crea Richiesta": "it", "Crear una Solicitud": "es", "Anfrage Erstellen": "de" };
+const LJS_li_requestApprove = { "Talep Onayla": "tr", "Approve Request": "en", "Approvare la Richiesta": "it", " Solicitud de Aprobación": "es", "Anforderung Genehmigen": "de" };
+const LJS_li_requestList = { "Talepler": "tr", "Request List": "en", "Lista delle Richieste": "it", "Lista de Solicitudes": "es", "Anfrageliste": "de" };
+
+const LJS_li_offer = { "Teklifler": "tr", "Offers": "en", "Offre": "it", "Ofrece": "es", "Angebote": "de" };
+const LJS_li_offerCreate = { "Teklif Gir": "tr", "Add Offer": "en", "Aggiungi Offerta": "it", "Añadir Oferta": "es", "Angebot Hinzufügen": "de" };
+const LJS_li_offerApprove = { "Teklif Onayla": "tr", "Approve Offer": "en", "Approvare l'Offerta": "it", "Aprobar Oferta": "es", "Angebot Genehmigen": "de" };
+const LJS_li_supplierCreate = { "Tedarikçi Ekle": "tr", "Add Supplier": "en", "Aggiungere Fornitore": "it", "Agregar Proveedor": "es", "Lieferanten Hinzufügen": "de" };
+const LJS_li_stockManagement = { "Stok İşlemleri": "tr", "Stock Operations": "en", "Operazioni di Stock": "it", "Operaciones Stock": "es", "Lagerbetrieb": "de" };
+
+const LJS_li_accounting = { "Muhasebe İşlemleri": "tr", "Accounting Operations": "en", "Operazioni Contabili": "it", "Operaciones de Contabilidad": "es", "Buchungsvorgänge": "de" };
+const LJS_li_invoiceEntry = { "Fatura Girişi": "tr", "Add Invoice": "en", "Aggiungere Fattura": "it", "Agregar Factura": "es", "Rechnung Hinzufügen": "de" };
+const LJS_li_invoices = { "Faturalar": "tr", "Invoices": "en", "Fatture": "it", "Facturas": "es", "Rechnungen": "de" };
+
+const LJS_li_requestReports = { "Talep Raporları": "tr", "Request Reports": "en", "Relazioni di Richiedere": "it", "Solicitar Informes": "es", "Berichte Anfordern": "de" };
+const LJS_li_processReports = { "Süreç Raporları": "tr", "Process Reports": "en", "Relazioni di Processo": "it", "Procesar Informes": "es", "Prozess Berichte": "de" };
+const LJS_li_transactions = { "Yapılan İşlemler": "tr", "Transactions": "en", "Transazioni": "it", "Transacciones": "es", "Transaktionen": "de" };
+const LJS_li_userHistory = { "Kullanıcı Geçmişi": "tr", "User History": "en", "Cronologia dell'Utente": "it", "Historial de Usuario": "es", "Benutzerverlauf": "de" };
+const LJS_companyName = { "Şirket": "tr", "Company": "en", "Azienda": "it", "Compañía": "es", "Unternehmen": "de" };
+
+// Index Page
+const LJS_I_toDoTitle = { "Kullanıcı Geçmişi": "tr", "User History": "en", "Cronologia dell'Utente": "it", "Historial de Usuario": "es", "Benutzerverlauf": "de" };
+
+
+
+
+function IndexLoadPage() {
+
+    document.getElementById("titletoDo").innerHTML = getTitle(LJS_I_toDoTitle);
+    document.getElementById("titletoDo").innerHTML = getTitle(LJS_I_toDoTitle);
+    document.getElementById("titletoDo").innerHTML = getTitle(LJS_I_toDoTitle);
+    document.getElementById("titletoDo").innerHTML = getTitle(LJS_I_toDoTitle);
+}
+function LoadPage() {
+    document.getElementById("title").innerHTML = getTitle(LJS_title_site);
+    document.getElementById("titlefooter").innerHTML = getTitle(LJS_footer_site);
+    document.getElementById("titleWelcome").innerHTML = getTitle(LJS_nav_welcome);
+    document.getElementById("titleMyAccount").innerHTML = getTitle(LJS_nav_myAccount);
+    document.getElementById("titleLogout").innerHTML = getTitle(LJS_nav_logout);
+    document.getElementById("titleSupport").innerHTML = getTitle(LJS_nav_support);
+    document.getElementById("titleLight").innerHTML = getTitle(LJS_nav_light);
+
+    document.getElementById("titleHome").innerHTML = getTitle(LJS_li_home);
+    document.getElementById("titleApps").innerHTML = getTitle(LJS_li_apps);
+    document.getElementById("titleMessage").innerHTML = getTitle(LJS_li_message);
+    document.getElementById("titleOffer").innerHTML = getTitle(LJS_li_offer);
+    document.getElementById("titleRequest").innerHTML = getTitle(LJS_li_request);
+    document.getElementById("titleCreateOffer").innerHTML = getTitle(LJS_li_offerCreate);
+    document.getElementById("titleApproveOffer").innerHTML = getTitle(LJS_li_offerApprove);
+    document.getElementById("titleCreateSupplier").innerHTML = getTitle(LJS_li_supplierCreate);
+    document.getElementById("titleRequestCreate").innerHTML = getTitle(LJS_li_requestCreate);
+    document.getElementById("titleRequestApprove").innerHTML = getTitle(LJS_li_requestApprove);
+    document.getElementById("titleRequestList").innerHTML = getTitle(LJS_li_requestList);
+    document.getElementById("titleStockManagement").innerHTML = getTitle(LJS_li_stockManagement);
+    document.getElementById("titleAccounting").innerHTML = getTitle(LJS_li_accounting);
+    document.getElementById("titleInvoiceEntry").innerHTML = getTitle(LJS_li_invoiceEntry);
+    document.getElementById("titleInvoices").innerHTML = getTitle(LJS_li_invoices);
+    document.getElementById("titleRequestReports").innerHTML = getTitle(LJS_li_requestReports);
+    document.getElementById("titleProcessReports").innerHTML = getTitle(LJS_li_processReports);
+    document.getElementById("titleTransactions").innerHTML = getTitle(LJS_li_transactions);
+    document.getElementById("titleSupport2").innerHTML = getTitle(LJS_nav_support);
+    document.getElementById("titleUserHistory").innerHTML = getTitle(LJS_li_userHistory);
+    
+    
+    
+    /*
+titleWelcome
+titleMyAccount
+titleLogout
+titleSupport
+titleLight
+
+ */
+}
+$(document).ready(function () {
+    LoadPage();
+});
+
+
+function LanguageChange(lg) {
+    LJS_Language = lg;
+    let country = "turkiye";
+    let language = "Türkçe";
+    switch (lg) {
+        case "tr": country = "turkiye"; language = "Türkçe"; break;
+        case "en": country = "england"; language = "English"; break;
+        case "es": country = "spain"; language = "Español"; break;
+        case "it": country = "italy"; language = "L'Italiano"; break;
+        case "de": country = "germany"; language = "Deutsch"; break;
+    }
+    let imgSrc = "/assets/images/flags/" + country + ".jpg";
+    document.getElementById("selectedFlag").src = imgSrc;
+    document.getElementById("selectedLanguage").innerHTML = language;
+    LoadPage();
+}
+
+
+
+
+
+function getTitle(obj) {
+    return Object.keys(obj).filter(key => obj[key] === LJS_Language);
+}
